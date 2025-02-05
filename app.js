9e57f2db805832c1d3c2d51e97e71fc2a8637809
phuongwduy <duyb2111790@student.ctu.edu.vn>
2025-02-05 20:13:33 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require('./app/api-error.js');
 const contactRouter = require('./app/routers/contact.router.js');
 
 const app = express();
@@ -11,5 +12,14 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application' });
 });
 app.use('/api/contacts', contactRouter);
+
+app.use((req, res, next) => {
+   return next(new ApiError(404, 'Resourse not found'));
+});
+
+app.use((err, req, res, next) => {
+   return res.status(err.statusCode || 500).json({ message: err.message || 'Internal server error' });
+});
+
 
 module.exports = app;
