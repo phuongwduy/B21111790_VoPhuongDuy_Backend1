96515a1655bfa4e1014cb26d07852fd42445db9c
phuongwduy <duyb2111790@student.ctu.edu.vn>
2025-02-05 17:39:02 +0700

    Cau hinh ESlint cho du an

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        node: true,
+        comonjs: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'prettier',
+    ],
+};
